4f77b08c878a6d9a53c802589c50bb543cf6fcf7
Yazan Alfarra <yalfarra@outlook.com>
2022-09-11 21:55:55 +0300

    responsive navbars

--- src/Components/HeaderAfterLogin.js
+++ src/Components/HeaderAfterLogin.js
@@ -9,19 +9,22 @@ import '../Styles/Header.css';
 class HeaderAfterLogin extends React.Component {
     render() {
       return (
-        <Navbar id="navigation" sticky="top">
+        <Navbar id="navigation" sticky="top" collapseOnSelect expand="lg">
         <Container>
           <Navbar.Brand id="logo"><Link style={{ textDecoration: 'none', color: 'black' }} to="/">BOOKEM</Link></Navbar.Brand>
-          <Nav className="ml-auto" id="items">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto justify-content-end" id="items" style={{ width: "100%" }}>
             <Nav.Link id="navitems"><Link  style={{ textDecoration: 'none', color: 'black' }} to="/">Home</Link></Nav.Link>
             <Nav.Link id="navitems"><Link  style={{ textDecoration: 'none', color: 'black' }} to="/aboutUs">About Us</Link></Nav.Link>
             <Nav.Link id="navitems"><Link  style={{ textDecoration: 'none', color: 'black' }} to="/ourTeam">Our Team</Link></Nav.Link>
             <Nav.Link id="navitems"><Link  style={{ textDecoration: 'none', color: 'black' }} to="/profile">Profile</Link></Nav.Link>
-            <Nav.Link id="navitems"><Link  style={{ textDecoration: 'none', color: 'black' }} to="/myCollections">My Collections</Link></Nav.Link>
+            <Nav.Link className="special" id="navitems"><Link  style={{ textDecoration: 'none', color: 'black' }} to="/myCollections">My Collections</Link></Nav.Link>
             <div className='headerbtn'>
             <LogoutButton />
             </div>
           </Nav>
+            </Navbar.Collapse>
         </Container>
       </Navbar>
         )
