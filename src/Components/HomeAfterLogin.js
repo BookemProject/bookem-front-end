469b72774018402fab71d23f16daf92df93ce44b
Yazan Alfarra <yalfarra@outlook.com>
2022-09-11 12:13:59 +0300

    filter bar

--- src/Components/HomeAfterLogin.js
+++ src/Components/HomeAfterLogin.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Filter from './Filter';
 
 class HomeAfterLogin extends React.Component {
     render() {
       return (
            <div>
-           Home After Login
+           <Filter />
            </div>
         )
     }
