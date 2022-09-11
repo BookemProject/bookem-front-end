f5749f735c31e8b80c60f3a5b092f431f177d940
Yazan Alfarra <yalfarra@outlook.com>
2022-09-11 22:35:55 +0300

    update

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
-// import '../Styles/Header.css';
+import '../Styles/Filter.css';
 
 class Filter extends React.Component {
+  
     render() {
       return (
-        <Nav justify variant="tabs">
+        <Nav justify variant="tabs" id="filter-tabs">
         <Nav.Item>
           <Nav.Link  style={{ textDecoration: 'none', color: 'black' }} eventKey="link-1">Ajloun</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link style={{ textDecoration: 'none', color: 'black' }} eventKey="link-2">Al Balqa</Nav.Link>
+          <Nav.Link style={{ textDecoration: 'none', color: 'black' }} eventKey="link-2">Amman</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link style={{ textDecoration: 'none', color: 'black' }} eventKey="link-3">Amman</Nav.Link>
+          <Nav.Link style={{ textDecoration: 'none', color: 'black' }} eventKey="link-3">Aqaba</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link style={{ textDecoration: 'none', color: 'black' }} eventKey="link-4">Aqaba</Nav.Link>
+          <Nav.Link style={{ textDecoration: 'none', color: 'black' }} eventKey="link-4">Balqa</Nav.Link>
         </Nav.Item>
         <Nav.Item>
           <Nav.Link style={{ textDecoration: 'none', color: 'black' }} eventKey="link-5">Irbid</Nav.Link>
