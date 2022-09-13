15e6fa69c992364cdebe3de87f00a0dcf62dda76
Yazan Alfarra <yalfarra@outlook.com>
2022-09-13 11:37:36 +0300

    css

--- src/Components/Figure.js
+++ src/Components/Figure.js
@@ -13,9 +13,9 @@ render(){
 
 <div id="AboutUs">
 <Card id ="myCard" style={{ width: "100%" , height:"100vh", border:"none", borderBottom: "1px solid gray", borderRadius: "0"  }}>
-      <Card.Img id="CardImg" style={{ width: '40%' , height:"auto", marginLeft:"90px", borderRadius:"20px" }} variant="top" src="https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
+      <Card.Img id="CardImg" style={{ width: '45%' , height:"auto", marginLeft:"90px", borderRadius:"20px" }} variant="top" src={require("../images/BOOKEM.webp")} />
       <Card.Body>
-        <div id = "CardTitle" ><h1 style={{textAlign:"left", paddingLeft: "40px"}}>Who Are We!</h1></div>
+        <div id = "CardTitle" ><h1 style={{textAlign:"left",paddingTop:"50px" }}>Who Are We!</h1></div>
         <hr/>
       <Card.Text id = "CardText">
       <p style={{textAlign:"justify",paddingRight:"75px" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed tellus ac dui ultrices rutrum. Nunc eget ligula vel tortor eleifend egestas. Aenean suscipit elit risus. Ut eget nunc quam. Sed eu nunc et dui condimentum dignissim nec quis metus. Donec nec lobortis eros. Sed pellentesque eu justo a pharetra. Pellentesque quis rutrum urna. Nulla lobortis ante ut urna hendrerit viverra. In porta, diam ut sagittis fringilla, ipsum purus rutrum arcu, vitae aliquam diam enim vel velit.</p>
