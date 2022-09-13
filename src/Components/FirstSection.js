98761e13307cdc258854bdb90af99ea722cf6705
ihababbas <ihababbas26@gmail.com>
2022-09-13 20:29:40 +0300

    aboutus1

--- src/Components/FirstSection.js
+++ src/Components/FirstSection.js
@@ -1,46 +1,49 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
-import Container from 'react-bootstrap/Container';
 import "../Styles/AboutUs.css";
 
 
 class FirstSection extends React.Component {
     render() {
-      return (
-        <div id="ABOUTUS">
-           <div className="SECTION">
-           <Container >
-        <Row className="row:after">
-        <Col sm={6} className="titleaboutus">
-      <h1> About Us </h1>
-       </Col>
-       <Col sm={6} className="titleaboutus">
-       <Card.Img variant="top" src={require("../images/BOOKEM.webp")} />
-       </Col>
-       </Row>
-        <div className='firstsection'>
-         <p className="paragraph1">
-         Bookem is a website that gives the user ease of use to search for farms anywhere in jordan, and create a booking service in the website To facilitate communication with Home/Farm owners.
-         </p>
-        <h3 className="subTitle"> Aims and objectives</h3>
-        <ul>
-      <li>Improving the quality of trips and travel by allowing people to find a place to travel as comfortable as home and at a lower cost than staying in hotels. </li>
-      
-      <li>The first website in Jordan for farm reservations </li>
-      </ul>
-    <h3 className="subTitle">Target Customer</h3>
-    <ul>
-   <li>
-     Everyone interested in traveling and trips</li>
-     <li> Home/Farm owners. </li> </ul>
-          </div>
-           </Container>
-           </div>
-           </div>
+   return(
+    <div>
+    <div>
+   <h1 > Who We Are and What We Do </h1>
+   <p >Bookem's mission is to connect both end point together by allowing state owners rent their sweet farms and hard working people rent to relax at the end of a full week. We focus on being the best commerce platform so our customers can focus on what matters most: growing their businesses and on the other
+     hand our beloved people relaxed. </p>
+
+     <Card.Img className="aboutusimage" variant="top" src={require("../images/window.png")} />
+   
+   </div>
+  <div className='row:after'>
+    <h1>Our Company Values</h1>
+    <p>
+    These are the core principles upon which Bookem was built, guiding what we do and how we do it. Each
+employee learns them, loves them and lives them. Our People benefit from them every time they use our
+Services or get help from our Support team.
+    </p>
+    <div className='column'>
+      <h3>CustomersFirst</h3>
+      <p>Our company exists to help people earn more or
+relax during the weekends. We make every
+decision and measure every outcome based on
+how well it serves our people</p>
+    </div>
+    <div className='column'>
+      <h3>Team on a Mission</h3>
+      <p>Bookem is made up of amazing individuals, but
+it’s only through teamwork that we achieve
+greatness. We’re committed to helping our
+customers by working together with equal parts
+humility and ambition to assure perfect
+experience transactions.</p>
+    </div>
+
+  </div>
+
+</div>
         )
     }
   }
