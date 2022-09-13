e344fecf25c317a4cd9bdc8797ad1ff93ab16e06
Yazan Alfarra <yalfarra@outlook.com>
2022-09-13 14:01:03 +0300

    update

--- src/Components/FirstSection.js
+++ src/Components/FirstSection.js
@@ -11,30 +11,36 @@ import "../Styles/AboutUs.css";
 class FirstSection extends React.Component {
     render() {
       return (
-        <Container >
+        <div id="ABOUTUS">
+           <div className="SECTION">
+           <Container >
         <Row className="row:after">
         <Col sm={6} className="titleaboutus">
       <h1> About Us </h1>
        </Col>
        <Col sm={6} className="titleaboutus">
-       <Card.Img variant="top" src={require("../images/logo.jpeg")} />
+       <Card.Img variant="top" src={require("../images/BOOKEM.webp")} />
        </Col>
        </Row>
         <div className='firstsection'>
-         <p>Bookem is a website that gives the user ease of use to search for farms anywhere in jordan, and create a booking service in the website To facilitate communication with Home/Farm owners.</p>
-        <h4> Aims and objectives</h4>
+         <p className="paragraph1">
+         Bookem is a website that gives the user ease of use to search for farms anywhere in jordan, and create a booking service in the website To facilitate communication with Home/Farm owners.
+         </p>
+        <h3 className="subTitle"> Aims and objectives</h3>
         <ul>
-      <li>	Improving the quality of trips and travel by allowing people to find a place to travel as comfortable as home and at a lower cost than staying in hotels. </li>
+      <li>Improving the quality of trips and travel by allowing people to find a place to travel as comfortable as home and at a lower cost than staying in hotels. </li>
       
-      <li>	The first website in Jordan for farm reservations </li></ul>
-    <h4>Target Customer</h4>
+      <li>The first website in Jordan for farm reservations </li>
+      </ul>
+    <h3 className="subTitle">Target Customer</h3>
     <ul>
    <li>
      Everyone interested in traveling and trips</li>
      <li> Home/Farm owners. </li> </ul>
           </div>
-          <Row> <span class="line" ></span></Row>
            </Container>
+           </div>
+           </div>
         )
     }
   }
--- src/Components/SacendSection.js
+++ src/Components/SacendSection.js
@@ -10,6 +10,8 @@ import "../Styles/AboutUs.css";
 class SacendSection extends React.Component {
     render() {
       return (
+        <div id="ABOUTUS1">
+        <div className="SECTION">
         <Container >
         <Row className="row:after">
            <Col sm={4} className="column">
@@ -17,13 +19,29 @@ class SacendSection extends React.Component {
              <Card.Img variant="top" className="sectionimg" src={require("../images/Rent.png")} />
              </Col>
            <Col sm={8} className="left">
-           <Row><h2>Farm Rental</h2></Row>
-           <Row><p> The site allows, by displaying all the pictures of the farm, their prices, and locations, to facilitate the customer's choice of the appropriate place for him on the appropriate date for him, in addition to the presence of a list of favorites that saves for the customer what he liked in case he wants to see it again</p></Row>
+           <Row><h3 className="subTitle">Farm Rental</h3></Row>
+           <Row><p className="paragraph1"> The site allows, by displaying all the pictures of the farm, their prices, and locations, to facilitate the customer's choice of the appropriate place for him on the appropriate date for him, in addition to the presence of a list of favorites that saves for the customer what he liked in case he wants to see it again</p></Row>
            
            </Col>
          </Row>
-         <Row> <span class="line" ></span></Row>
          </Container>
+         <Container >
+     
+        <Row className="row:after">
+         
+           <Col sm={8} className="left" >
+          <Row><h3 className="subTitleR">Add Farms</h3></Row>
+          <Row><p className="paragraphR"> The site provides distinguished marketing services as it helps in publishing the name of your farm through a form to be filled out by the lessor and adding all the necessary and important information to the customer</p></Row>
+
+          </Col>
+          <Col sm={4} className="column" >
+          <Card.Img variant="top" className="sectionimg" src={require("../images/marketing.png")} />
+            </Col>
+        </Row>
+        </Container>
+         </div>
+         </div>
+
         )
     }
   }
